37cabcb2ff56448c5c275011980eb780fd4b258a
gtomchuk2005 <gtomchuk2005@gmail.com>
2023-01-09 19:24:49 -0500

    finished classwork

--- subchapter2.js
+++ subchapter2.js
@@ -1,6 +1,10 @@
 var fs = require("fs");
 
-var data = fs.readFileSync('input.txt');
+fs.readFile('input.txt', function (err, data)
+{
+    if (err)
+        return console.log(err);
+    console.log(data.toString());
+});
 
-console.log(data.toString());
 console.log("Program Ended");
